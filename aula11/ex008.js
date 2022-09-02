2bc311ded717e3b36f8bdb1003a71fa633973ecd
Walter Silva <walter4web@gmail.com>
2022-09-02 10:09:45 -0300

    initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 53;
+console.log(`A velocidade do carro é ${vel}km/h`);
+
+if (vel > 60) {
+  console.log("Você ultrapassou a velocidade permitida. Multado!!");
+}
+
+console.log("Dirija sempre com cinto de segurança");
